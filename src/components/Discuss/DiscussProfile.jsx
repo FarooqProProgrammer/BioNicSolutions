27f5b3822e54905b7245c165e00756324303425e
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2023-10-22 16:47:11 +0500

    Updated

--- src/components/Discuss/DiscussProfile.jsx
+++ src/components/Discuss/DiscussProfile.jsx
@@ -6,7 +6,7 @@ export default function DiscussProfile() {
     return (
         <section className="text-gray-600 body-font h-full">
             <div className="container mx-auto flex px-5 py-4 md:flex-row flex-col items-center">
-                <div className="lg:w-full md:w-1/2 w-5/6 h-[350px] mb-10 md:mb-0">
+                <div className="lg:w-full md:w-1/2 h-[350px] mb-10 md:mb-0">
                         <img
                             className="object-cover object-center h-full rounded"
                             alt="hero"
@@ -14,7 +14,7 @@ export default function DiscussProfile() {
                         />
                         
                 </div>
-                <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                <div className="lg:flex-grow lg:w-full md:w-1/2 flex flex-col md:items-start md:text-left items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
                         Before they sold out
                         <br className="hidden lg:inline-block" />
